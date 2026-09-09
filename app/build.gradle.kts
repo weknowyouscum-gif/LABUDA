e322b3f34edcf76db93048bdceefccf57ed9c537
WKYS <weknowyouscum@gmail.com>
2026-09-09 22:36:38 +0300

    LABUDA 0.40

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.labuda.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "0.39"
+        versionCode = 40
+        versionName = "0.40"
     }
 
     compileOptions {
