562426dc1744a15e79daaeaa313b2b4fb9d85225
WKYS <weknowyouscum@gmail.com>
2026-09-09 16:54:06 +0300

    Set app version to 0.32 for run 32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.labuda.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 32
+        versionName = "0.32"
     }
 
     compileOptions {
