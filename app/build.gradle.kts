4b8d6d0d8a50f654ef38866ddf0976fb900b7a66
WKYS <weknowyouscum@gmail.com>
2026-09-10 21:39:30 +0300

    Bump LABUDA to version 0.45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.labuda.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 44
-        versionName = "0.44"
+        versionCode = 45
+        versionName = "0.45"
     }
 
     compileOptions {
